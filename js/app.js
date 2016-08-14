5252fe65ca6317afe2473cecfd84651d4f4f337a
Ina Maloney <Ina@Inas-MacBook-Air.local>
2016-08-14 12:23:17 -0400

    Applied knockout bindings

--- js/app.js
+++ js/app.js
@@ -3,41 +3,41 @@ section of this course.*/
 // Model
 var summits = [
   // 1000ft plus
-  {title: 'Cadillac Mountain', location: {lat: 44.352643, lng: -68.224610}, group: '5'},
-  {title: 'Sargent Mountain', location: {lat: 44.3425796, lng: -68.2727932}, group: '5'},
-  {title: 'Dorr Mountain', location: {lat: 44.3545241, lng: -68.215569}, group: '5'},
-  {title: 'Pemetic Mountain', location: {lat: 44.335082, lng: -68.243838}, group: '5'},
-  {title: 'Penobscot Mountain', location: {lat: 44.332858, lng: -68.266682}, group: '5'},
-  {title: 'Bernard Mountain', location: {lat: 44.300259, lng: -68.37252}, group: '5'},
-  {title: 'Champlain Mountain', location: {lat: 44.3506355, lng: -68.1930676}, group: '5'},
-  {title: 'Gilmore Peak', location: {lat: 44.338413, lng: -68.279182}, group: '5'},
+  {title: 'Cadillac Mountain', location: {lat: 44.352643, lng: -68.224610}, group: '1000ft and up'},
+  {title: 'Sargent Mountain', location: {lat: 44.3425796, lng: -68.2727932}, group: '1000ft and up'},
+  {title: 'Dorr Mountain', location: {lat: 44.3545241, lng: -68.215569}, group: '1000ft and up'},
+  {title: 'Pemetic Mountain', location: {lat: 44.335082, lng: -68.243838}, group: '1000ft and up'},
+  {title: 'Penobscot Mountain', location: {lat: 44.332858, lng: -68.266682}, group: '1000ft and up'},
+  {title: 'Bernard Mountain', location: {lat: 44.300259, lng: -68.37252}, group: '1000ft and up'},
+  {title: 'Champlain Mountain', location: {lat: 44.3506355, lng: -68.1930676}, group: '1000ft and up'},
+  {title: 'Gilmore Peak', location: {lat: 44.338413, lng: -68.279182}, group: '1000ft and up'},
   // 700 to 999
-  {title: 'Bald Peak', location: {lat: 44.335095, lng: -68.283668}, group: '4'},
-  {title: 'Mansell Mountain', location: {lat: 44.307024, lng: -68.360295}, group: '4'},
-  {title: 'Cedar Swamp Mountain', location: {lat: 44.328413, lng: -68.275849}, group: '4'},
-  {title: 'Knight Nubble', location: {lat: 44.304524, lng: -68.370018}, group: '4'},
-  {title: 'Parkman Mountain', location: {lat: 44.338253, lng: -68.285903}, group: '4'},
-  {title: 'North Bubble', location: {lat: 44.344246, lng: -68.256682}, group: '4'},
-  {title: 'Norumbega Mountain', location: {lat: 44.324071, lng: -68.295601}, group: '4'},
-  {title: 'Beech Mountain', location: {lat: 44.311469, lng: -68.346128}, group: '4'},
-  {title: 'South Bubble', location: {lat: 44.338691, lng: -68.254182}, group: '4'},
-  {title: 'Huguenot Head', location: {lat: 44.353135, lng: -68.199457}, group: '4'},
-  {title: 'McFarland Mountain', location: {lat: 44.38202, lng: -68.264953}, group: '4'},
+  {title: 'Bald Peak', location: {lat: 44.335095, lng: -68.283668}, group: '700ft - 999ft'},
+  {title: 'Mansell Mountain', location: {lat: 44.307024, lng: -68.360295}, group: '700ft - 999ft'},
+  {title: 'Cedar Swamp Mountain', location: {lat: 44.328413, lng: -68.275849}, group: '700ft - 999ft'},
+  {title: 'Knight Nubble', location: {lat: 44.304524, lng: -68.370018}, group: '700ft - 999ft'},
+  {title: 'Parkman Mountain', location: {lat: 44.338253, lng: -68.285903}, group: '700ft - 999ft'},
+  {title: 'North Bubble', location: {lat: 44.344246, lng: -68.256682}, group: '700ft - 999ft'},
+  {title: 'Norumbega Mountain', location: {lat: 44.324071, lng: -68.295601}, group: '700ft - 999ft'},
+  {title: 'Beech Mountain', location: {lat: 44.311469, lng: -68.346128}, group: '700ft - 999ft'},
+  {title: 'South Bubble', location: {lat: 44.338691, lng: -68.254182}, group: '700ft - 999ft'},
+  {title: 'Huguenot Head', location: {lat: 44.353135, lng: -68.199457}, group: '700ft - 999ft'},
+  {title: 'McFarland Mountain', location: {lat: 44.38202, lng: -68.264953}, group: '700ft - 999ft'},
   // 500 to 699
-  {title: 'The Triad', location: {lat: 44.320913, lng: -68.236959}, group: '3'},
-  {title: 'Acadia Mountain', location: {lat: 44.323746, lng: -68.317482}, group: '3'},
-  {title: 'Youngs Mountain', location: {lat: 44.391746, lng: -68.266404}, group: '3'},
-  {title: 'St. Sauveur Mountain', location: {lat: 44.309524, lng: -68.321683}, group: '3'},
-  {title: 'Conners Nubble', location: {lat: 44.356263, lng: -68.256524}, group: '3'},
-  {title: 'Day Mountain', location: {lat: 44.31008, lng: -68.231681}, group: '3'},
-  {title: 'Valley Peak', location: {lat: 44.30508, lng: -68.319183}, group: '3'},
-  {title: 'Gorham Mountain', location: {lat: 44.326895, lng: -68.193268}, group: '3'},
-  {title: 'The Beehive', location: {lat: 44.333414, lng: -68.188345}, group: '3'},
+  {title: 'The Triad', location: {lat: 44.320913, lng: -68.236959}, group: '500ft - 699ft'},
+  {title: 'Acadia Mountain', location: {lat: 44.323746, lng: -68.317482}, group: '500ft - 699ft'},
+  {title: 'Youngs Mountain', location: {lat: 44.391746, lng: -68.266404}, group: '500ft - 699ft'},
+  {title: 'St. Sauveur Mountain', location: {lat: 44.309524, lng: -68.321683}, group: '500ft - 699ft'},
+  {title: 'Conners Nubble', location: {lat: 44.356263, lng: -68.256524}, group: '500ft - 699ft'},
+  {title: 'Day Mountain', location: {lat: 44.31008, lng: -68.231681}, group: '500ft - 699ft'},
+  {title: 'Valley Peak', location: {lat: 44.30508, lng: -68.319183}, group: '500ft - 699ft'},
+  {title: 'Gorham Mountain', location: {lat: 44.326895, lng: -68.193268}, group: '500ft - 699ft'},
+  {title: 'The Beehive', location: {lat: 44.333414, lng: -68.188345}, group: '500ft - 699ft'},
   // 500ft and under
-  {title: 'Kebo Mountain', location: {lat: 44.373413, lng: -68.218347}, group: '2'},
-  {title: 'Bald Mountain', location: {lat: 44.294524, lng: -68.384185}, group: '2'},
-  {title: 'Flying Mountain', location: {lat: 44.304876, lng: -68.313323}, group: '2'},
-  {title: 'Great Head', location: {lat: 44.328412, lng: -68.174172}, group: '2'}
+  {title: 'Kebo Mountain', location: {lat: 44.373413, lng: -68.218347}, group: '499ft and under'},
+  {title: 'Bald Mountain', location: {lat: 44.294524, lng: -68.384185}, group: '499ft and under'},
+  {title: 'Flying Mountain', location: {lat: 44.304876, lng: -68.313323}, group: '499ft and under'},
+  {title: 'Great Head', location: {lat: 44.328412, lng: -68.174172}, group: '499ft and under'}
 ];
 
 // Initialize the map using google maps api
@@ -53,6 +53,7 @@ function initMap() {
   ko.applyBindings(new ViewModel());
 }
 
+
 // Function to grab photos from flickr
 // Modified code to work how I wanted from http://api.jquery.com/jquery.getjson/
 function inputImage(tag) {
@@ -62,7 +63,7 @@ function inputImage(tag) {
     tagmode: "any",
     format: "json"
   })
-    .success(function(data) {
+    .done(function(data) {
         console.log("posted");
         $.each(data.items, function( i, item ) {
           $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
@@ -76,6 +77,7 @@ function inputImage(tag) {
       // acadiaGeneric.jpg from http://atravelinfos.com/acadia-national-park-see-the-peregrines-and-natural-beauty.html
       // Photo by Aleen Robert
       $( "<img>" ).attr({src: 'imgs/acadiaGeneric.jpg', alt: 'Acadia National Park'}).appendTo( "#images" );
+      $("<p>Request failed</p>").appendTo("#images");
   });
 }
 
@@ -107,6 +109,7 @@ function toggleBounce(marker) {
   }
 }
 
+var dropdownOptions = ['All', '499ft and under', '500ft - 699ft', '700ft - 999ft', '1000ft and up'];
 // View Model
 function ViewModel() {
   var largeInfowindow = new google.maps.InfoWindow();
@@ -140,6 +143,8 @@ function ViewModel() {
   // Extend the boundaries of the map for each marker
   map.fitBounds(bounds);
 
+  this.filterText = ko.observableArray(dropdownOptions);
+  this.selectedValue = ko.observable('1');
   // if a summit is clicked in the list view bring up the infowindow
   openWindow = function() {
     // make all other marker animations null
@@ -152,11 +157,10 @@ function ViewModel() {
   // TODO: Get any open infowindows to exit
   // filter through the list based on the dropdown value
   filter = function() {
-    var dropdown = document.getElementById('dificulty');
-    var dropdownValue = dropdown.options[dropdown.selectedIndex].value;
+    dropdownValue = this.selectedValue();
     this.locations.removeAll();
     for (var i = 0; i < summits.length; i++) {
-      if (dropdownValue === '1'){
+      if (dropdownValue === 'All'){
         this.locations.push(summits[i]);
         summits[i].marker.setVisible(true);
       }
